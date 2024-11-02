c4954f84c0420565cd1dfaa36e479432d2e8b5fe
Andrew Everman <andreweverman@gmail.com>
2024-11-02 10:44:57 -0400

    making it all one doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
+use std::collections::HashMap;
+
 use mongodb::bson::DateTime;
 use serde::{Deserialize, Serialize};
+use strum_macros::{Display, EnumString};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Dig {
-    #[serde(rename = "_id")]
-    pub id: String,
-    pub user_id: String,
     pub playlist_id: String,
     pub last_run: DateTime,
     pub running: bool,
@@ -15,46 +15,81 @@ pub struct Dig {
     pub album_sort: bool,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Dug {
-    #[serde(rename = "_id")]
-    pub id: String,
-    pub user_id: String,
     pub playlist_id: String,
     pub last_run: DateTime,
     pub running: bool,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Catalog {
-    #[serde(rename = "_id")]
-    pub id: String,
-    pub user_id: String,
     pub catalog_playlist_id: String,
     pub discover_weekly_playlist_id: String,
     pub last_run: DateTime,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct AlbumSaveTracks {
-    #[serde(rename = "_id")]
-    pub id: String,
-    pub user_id: String,
     pub last_run: DateTime,
+}
+
+#[derive(EnumString, Debug, Display, Deserialize, Serialize, Clone, PartialEq, Eq, Hash)]
+pub enum Services {
+    #[strum(serialize = "dig")]
+    Dig,
+    #[strum(serialize = "dug")]
+    Dugs,
+    #[strum(serialize = "catalog")]
+    Catalogs,
+    #[strum(serialize = "albumSaveTracks")]
+    AlbumSaveTracks,
+}
+
+pub trait Service: Serialize + for<'de> Deserialize<'de> + Clone {}
+
+impl Service for Dig {}
+impl Service for Dug {}
+impl Service for Catalog {}
+impl Service for AlbumSaveTracks {}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+#[serde(tag = "type", content = "data")]
+pub enum ServiceEnum {
+    Dig(Dig),
+    Dug(Dug),
+    Catalog(Catalog),
+    AlbumSaveTracks(AlbumSaveTracks),
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct User {
     #[serde(rename = "_id")]
-    pub id: String,
-    pub user_id: String,
-    pub display_name: String,
-    pub username: String,
+    pub id: Option<String>,
+    pub user_id: Option<String>,
+    pub display_name: Option<String>,
+    pub username: Option<String>,
     pub photo: Option<String>,
     pub access_token: Option<String>,
     pub refresh_token: Option<String>,
+    pub service_list: Vec<Services>,
+    pub services: HashMap<Services, ServiceEnum>,
+}
+
+impl User {
+    pub fn get_service<T: Service>(&self, service: Services) -> Option<T>
+    where
+        T: From<Dig> + From<Dug> + From<Catalog> + From<AlbumSaveTracks>,
+    {
+        self.services.get(&service).map(|s| match s {
+            ServiceEnum::Dig(d) => d.clone().into(),
+            ServiceEnum::Dug(d) => d.clone().into(),
+            ServiceEnum::Catalog(d) => d.clone().into(),
+            ServiceEnum::AlbumSaveTracks(d) => d.clone().into(),
+        })
+    }
 }
